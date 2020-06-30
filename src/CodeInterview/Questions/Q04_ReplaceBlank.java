3d5fffde076c67915b3626f0ed65975b2138a3a8
bobshui <szw199408@sina.com>
2020-06-30 23:10:04 +0800

    20200630学习

--- src/CodeInterview/Questions/Q04_ReplaceBlank.java
+++ src/CodeInterview/Questions/Q04_ReplaceBlank.java
@@ -4,38 +4,38 @@ package CodeInterview.Questions;
 题目：请实现一个函数，把字符串中的每个空格替换成"20%"。例如输入"We are happy"，则输出"We20%are20%happy"
  */
 
+//刚拿到确实没什么想法，直接替换？
+//比如，直接遍历，用Stringbuilder去加起来？
+//哦，要在原字符串上面弄，假设情况只允许一份空间，直接遍历，会需要O(n*n)的挪动，可以计算所需空间从后往前！
+//但仔细一想，Java的字符串似乎没有空间连续的说法？只能作为一个思路了
+
 public class Q04_ReplaceBlank {
 
     public static void main(String[] args) {
-        int[][] matrix = new int[][]{{1, 2, 3, 4}, {5, 6, 7, 8}, {11, 12, 13, 14}, {15, 16, 17, 18}};
-        System.out.println(solution1(matrix, 100));
+        String s = "ddd  as asdas ";
+        System.out.println(solution1(s));
     }
 
     /**
-     * 时间复杂度为O(m+n)
-     * @param matrix 数组
-     * @param number 待查找待数字
+     * 利用StringBuilder直接相加，这个解法感觉意义不大？从时间复杂度来看是O(n)
+     * @param s 输入字符串
      * @return
      */
-    public static boolean solution1(int[][] matrix, int number) {
-        //行列，暂不考虑特殊情况
-        int row = matrix.length;
-        int col = matrix[0].length;
-        int i = 0;
-        int j = col - 1;
-        while (i < row && j > -1) {
-            if (matrix[i][j] > number) {
-                j -= 1;
-            } else if (matrix[i][j] < number) {
-                i += 1;
-            } else {
-                return true;
+    public static String solution1(String s) {
+        //暂不考虑特殊情况
+        char[] c = s.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        for(char ch: c){
+            if(ch == ' '){
+                sb.append("20%");
+            }
+            else{
+                sb.append(ch);
             }
         }
-        return false;
+        return sb.toString();
     }
 
-    //考虑到每一行都是有序的数组，也可以对每一行进行二分查找，这样的时间复杂度为O(mlogn)
 }
 
 
