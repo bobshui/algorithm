f3b19c3167aa9593b1e2ba706168623cd9e6163a
bobshui <bobshui@163.com>
2020-06-30 19:34:44 +0800

    20200630学习

--- src/CodeInterview/Questions/Q03_FindInPartiallySortedMatrix.java
+++ src/CodeInterview/Questions/Q03_FindInPartiallySortedMatrix.java
@@ -5,10 +5,42 @@ package CodeInterview.Questions;
     请完成一个函数，输入这样的一个二维数组和整数，判断数组中是否有该整数。
  */
 
-public class Q03_FindInPartiallySortedMatrix{
-    public static void solution1() {
+//一开始确实还是没想到什么好办法，因为这个和什么数据结构和算法感觉都扯不上关系，如果左上右下的来话，基本走不通，
+//还是看了答案才有的思路
+//输入就略了，直接生成二维数组
 
+public class Q03_FindInPartiallySortedMatrix {
+
+    public static void main(String[] args) {
+        int[][] matrix = new int[][]{{1, 2, 3, 4}, {5, 6, 7, 8}, {11, 12, 13, 14}, {15, 16, 17, 18}};
+        System.out.println(solution1(matrix, 100));
     }
+
+    /**
+     * 时间复杂度为O(m+n)
+     * @param matrix 数组
+     * @param number 待查找待数字
+     * @return
+     */
+    public static boolean solution1(int[][] matrix, int number) {
+        //行列，暂不考虑特殊情况
+        int row = matrix.length;
+        int col = matrix[0].length;
+        int i = 0;
+        int j = col - 1;
+        while (i < row && j > -1) {
+            if (matrix[i][j] > number) {
+                j -= 1;
+            } else if (matrix[i][j] < number) {
+                i += 1;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //考虑到每一行都是有序的数组，也可以对每一行进行二分查找，这样的时间复杂度为O(mlogn)
 }
 
 
