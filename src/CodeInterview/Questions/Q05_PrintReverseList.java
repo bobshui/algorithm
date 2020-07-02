1ec1601fb8b5d4144526b6edd51af5454de04fbe
bobshui <bobshui@163.com>
2020-07-02 20:20:20 +0800

    20200702学习

--- src/CodeInterview/Questions/Q05_PrintReverseList.java
+++ src/CodeInterview/Questions/Q05_PrintReverseList.java
@@ -1,27 +1,82 @@
 package CodeInterview.Questions;
 
 /*
-题目：请实现一个函数，把字符串中的每个空格替换成"20%"。例如输入"We are happy"，则输出"We20%are20%happy"
+题目：输入一个链表的头节点，从头到尾反过来打印每个节点的值
  */
+
+import java.util.Stack;
 
 public class Q05_PrintReverseList {
 
     public static void main(String[] args) {
-        String s = "ddd  as asdas ";
-        System.out.println(solution1(s));
+        LinkList l0 = new LinkList("l0", null);
+        LinkList l1 = new LinkList("l1", l0);
+        LinkList l2 = new LinkList("l2", l1);
+        LinkList l3 = new LinkList("l3", l2);
+        solution2(l3);
+    }
+
+    //定义LinkList
+    //---内部类问题
+    static class LinkList {
+        public String name;
+        public LinkList next; //指向下一个节点
+
+        //构造器
+        public LinkList(String name, LinkList next) {
+            this.name = name;
+            this.next = next;
+        }
+
+        @Override
+        public String toString() {
+            return "LinkList [name=" + name + "]";
+        }
+
+        public void printAll() {
+            System.out.print(this.toString() + "--");
+            LinkList ll = this;
+            while (ll.next != null) {
+                ll = ll.next;
+                System.out.print(ll.toString() + "--");
+            }
+            System.out.println();
+        }
     }
 
     /**
-     * 利用StringBuilder直接相加，这个解法感觉意义不大？从时间复杂度来看是O(n)
-     * @param s 输入字符串
-     * @return
+     * 自己想的一个办法，就是遍历，遍历一个节点就放一个节点到末尾
+     * 看答案后意识到，(1)破坏了原有链表到结构 (2)并没有完全满足需求，应该直接打印而不是返回新的头节点
+     * @param ll 链表的头节点
+     * @return 新链表的头节点
      */
-    public static String solution1(String s) {
+    public static LinkList solution1(LinkList ll) {
         //暂不考虑特殊情况
-        char[] c = s.toCharArray();
-        StringBuilder sb = new StringBuilder();
+        LinkList newhead = ll;
+        LinkList cur = newhead.next;
+        newhead.next = null;
+        LinkList temp;
+        while (cur != null) {
+            temp = cur.next;
+            cur.next = newhead;
+            newhead = cur;
+            cur = temp;
+        }
+        return newhead;
+    }
 
-        return sb.toString();
+    public static void solution2(LinkList ll){
+        Stack<LinkList> stack = new Stack<>();
+        //暂不考虑特殊情况
+        while(ll!=null){
+            stack.push(ll);
+            ll=ll.next;
+        }
+        //while(stack.size()>0){，不优秀的遍历
+        while(!stack.empty()){
+            LinkList l = stack.pop();
+            System.out.print(l.toString());
+        }
     }
 
 }
