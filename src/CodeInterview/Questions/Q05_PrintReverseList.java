7af83b538a687c88347df708eded05a8b14dcce0
bobshui <bobshui@163.com>
2020-07-03 20:37:37 +0800

    20200703学习

--- src/CodeInterview/Questions/Q05_PrintReverseList.java
+++ src/CodeInterview/Questions/Q05_PrintReverseList.java
@@ -13,7 +13,8 @@ public class Q05_PrintReverseList {
         LinkList l1 = new LinkList("l1", l0);
         LinkList l2 = new LinkList("l2", l1);
         LinkList l3 = new LinkList("l3", l2);
-        solution2(l3);
+        //solution2(l3);
+        solution3(l1, null);
     }
 
     //定义LinkList
@@ -47,6 +48,7 @@ public class Q05_PrintReverseList {
     /**
      * 自己想的一个办法，就是遍历，遍历一个节点就放一个节点到末尾
      * 看答案后意识到，(1)破坏了原有链表到结构 (2)并没有完全满足需求，应该直接打印而不是返回新的头节点
+     *
      * @param ll 链表的头节点
      * @return 新链表的头节点
      */
@@ -65,20 +67,52 @@ public class Q05_PrintReverseList {
         return newhead;
     }
 
-    public static void solution2(LinkList ll){
+//    是不是很简单整洁？
+//    public static Node reverseList(Node node) {
+//        Node pre = null;
+//        Node next = null;
+//        while (node != null) {
+//            next = node.next;
+//            node.next = pre;
+//            pre = node;
+//            node = next;
+//        }
+//        return pre;
+//    }
+
+    /**
+     * 利用栈的性质，感受到先进后出到逻辑后，应该要能够联想到栈到结构
+     *
+     * @param ll
+     */
+    public static void solution2(LinkList ll) {
         Stack<LinkList> stack = new Stack<>();
         //暂不考虑特殊情况
-        while(ll!=null){
+        while (ll != null) {
             stack.push(ll);
-            ll=ll.next;
+            ll = ll.next;
         }
         //while(stack.size()>0){，不优秀的遍历
-        while(!stack.empty()){
+        while (!stack.empty()) {
             LinkList l = stack.pop();
             System.out.print(l.toString());
         }
     }
 
+    /**
+     * 递归本质上是栈结构（感受下这个思想）
+     * 但是调用太深，可能会是个问题
+     * @param ll
+     */
+    public static void solution3(LinkList ll, LinkList pre) {
+        //暂不考虑特殊情况
+        if (ll == null) {
+            return;
+        }
+        LinkList next = ll.next;
+        ll.next = pre;
+        solution3(next, ll);
+    }
 }
 
 
