0805aedf3ad949a88708963c897c98fa872bc813
bobshui <bobshui@163.com>
2020-07-05 13:40:37 +0800

    20200704学习

---
+++ src/CodeInterview/Questions/Q06_CounstructBinaryTree.java
@@ -0,0 +1,48 @@
+package CodeInterview.Questions;
+
+/*
+题目：输入某二叉树的前序遍历和中序遍历的结果，请重建出该二叉树。假设输入的前序遍历和中序遍历的结果中都不含重复的数字。
+例如输入前序遍历序列{1,2,4,7,3,5,6,8}和中序遍历{4,7,2,1,5,3,8,6}，则重建出图2.6所示的二叉树并输出它的头节点。
+        1
+    2       3
+4         5     6
+  7           8
+
+ */
+
+import java.util.Stack;
+
+public class Q06_CounstructBinaryTree {
+
+    //定义LinkList
+    //---内部类问题
+    static class LinkList {
+        public String name;
+        public LinkList next; //指向下一个节点
+
+        //构造器
+        public LinkList(String name, LinkList next) {
+            this.name = name;
+            this.next = next;
+        }
+
+        @Override
+        public String toString() {
+            return "LinkList [name=" + name + "]";
+        }
+
+        public void printAll() {
+            System.out.print(this.toString() + "--");
+            LinkList ll = this;
+            while (ll.next != null) {
+                ll = ll.next;
+                System.out.print(ll.toString() + "--");
+            }
+            System.out.println();
+        }
+    }
+
+
+}
+
+
