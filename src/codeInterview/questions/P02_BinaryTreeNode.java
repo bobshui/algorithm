def72a6151da460717b51cb7c84a6e309c3a0764
bobshui <szw199408@sina.com>
2020-07-11 18:19:27 +0800

    20200711学习

---
+++ src/codeInterview/questions/P02_BinaryTreeNode.java
@@ -0,0 +1,11 @@
+package codeInterview.questions;
+
+public class P02_BinaryTreeNode {
+    public int data;
+    public P02_BinaryTreeNode leftChild;
+    public P02_BinaryTreeNode rightChild;
+
+    public P02_BinaryTreeNode(int data){
+        this.data = data;
+    }
+}
