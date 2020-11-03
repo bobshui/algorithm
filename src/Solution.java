0ea2c6f12848378c363b3319d58038e0282712f7
bobshui <bobshui@163.com>
2020-11-03 18:38:13 +0800

    20201103学习

--- src/Solution.java
+++ src/Solution.java
@@ -4,12 +4,68 @@ import java.util.*;
 public class Solution {
 
     public static void main(String[] args) {
-        
+        int[] a = {1,2,4,4,5};
+    }
+
+    /**
+     * Definition for singly-linked list.
+     * class ListNode {
+     *     int val;
+     *     ListNode next;
+     *     ListNode(int x) {
+     *         val = x;
+     *         next = null;
+     *     }
+     * }
+     */
+    class ListNode {
+          int val;
+          ListNode next;
+          ListNode(int x) {
+              val = x;
+              next = null;
+          }
+      }
+
+    public boolean hasCycle(ListNode head) {
+        return true;
     }
 }
 
+//3. 20201103 牛客
+///**
+// * 二分查找
+// * @param n int整型 数组长度
+// * @param v int整型 查找值
+// * @param a int整型一维数组 有序数组
+// * @return int整型
+// */
+//public static int upper_bound_ (int n, int v, int[] a) {
+//    int low = 0;
+//    int high = n -1;
+//    if(a[0] >= v){
+//        return 1;
+//    }
+//    if(a[high] < v){
+//        return n + 1;
+//    }
+//    while(low <= high){
+//        int mid = (low + high) / 2;
+//        if(a[mid] < v){
+//            if(a[mid+1] == v){
+//                return mid + 2;
+//            }
+//            low = (low + high) / 2 + 1;
+//        }
+//        else{
+//            high = (low + high) / 2 - 1;
+//        }
+//    }
+//    return n + 1;
+//}
+//不够简练，不过思路基本正确，读题还需要细心一些
 
-//2.设计LRU缓存结构 20201002 牛客
+//2.设计LRU缓存结构 20201102 牛客
 ///**
 // * lru design
 // * @param operators int整型二维数组 the ops
